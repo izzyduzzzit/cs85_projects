1023b57e8f0a2f9b5d11b14f762adf5cecdb605f
Izarra Villareal <izarravill@gmail.com>
2026-07-11 13:32:37 -0700

    Addition to exclude module4b/ and updated routes in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return "This is Izarra's first Laravel app!";
 });
+
+Route::get('/cs85_module2a/price_engine.php', function () {
+    require base_path('cs85_module2a/price_engine.php');
+});
+
+Route::get('/cs85_module2a/price_engine_refactored.php', function () {
+    require base_path('cs85_module2a/price_engine_refactored.php');
+});
+
+Route::get('/cs85_module2b/cosmic_calendar.php', function () {
+    require base_path('cs85_module2b/cosmic_calendar.php');
+});
+
+Route::get('/module4b/show_inventory.php', function () {
+    require base_path('module4b/show_inventory.php');
+});
